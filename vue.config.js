63e1ec6d5549629d8145e01a68370281b7214e19
ppenghui <18893183061@163.com>
2021-08-31 10:56:23 +0800

    ..

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    publicPath: '/',//publicPath取代了baseUrl
+    outputDir: 'docs',
+    lintOnSave: true,
+    runtimeCompiler: true, //关键点在这  原来的 Compiler 换成了 runtimeCompiler
+    // 调整内部的 webpack 配置。
+    // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli/webpack.md
+    chainWebpack: () => { },
+    configureWebpack: () => { },
+    // 配置 webpack-dev-server 行为。
+    devServer: {
+        open: true,
+        host: '0.0.0.0',
+        port: 8080,
+        https: false,
+        hotOnly: false,
+        // 查阅 https://github.com/vuejs/vue-doc-zh-cn/vue-cli/cli-service.md#配置代理
+        proxy: null, // string | Object
+        before: app => { }
+    }
+}
