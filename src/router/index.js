19a1375e4ba67b3f23840a0b284e85ef1a4f2e12
ppenghui <18893183061@163.com>
2021-10-25 16:31:04 +0800

    g

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '@/views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,12 +8,12 @@ const routes = [
   {
     path: '/',
     redirect: 'home',
-    component: () => import('../views/page/index.vue'),
+    component: () => import('@/views/page/index.vue'),
     children: [
       {
         path: 'home',
         name: 'Home',
-        component: Home,
+        component: () => import('@/views/page/index.vue'),
       },
     ]
   },
